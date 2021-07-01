821a77bf4eeacaf6b86c55e350d2f2a86f6fec5e
Vicharm <vic010744@gmail.com>
2021-07-01 18:01:17 +0800

    user can add course into shopping cart

--- components/FundraisingCourses.vue
+++ components/FundraisingCourses.vue
@@ -1,54 +1,153 @@
 <template>
   <div class="flex flex-col">
     <div>正在募資的課程</div>
-    <div class="grid grid-cols-4 gap-4">
-      <div
-        v-for="course in courses"
-        :key="course.id"
-      >
-        <div class="image-box">
-          <img
-            :src="course.image"
-            alt=""
-            class="object-contain w-full"
+    <div class="grid grid-cols-3 gap-4">
+      <div v-for="course in courses" :key="course.id">
+        <div class="image-box relative cursor-pointer">
+          <img :src="course.image" alt="" class="object-contain w-full" />
+          <div
+            class="
+              absolute
+              bottom-0
+              left-0
+              w-full
+              h-full
+              transition-opacity
+              duration-500
+              rounded-b-none
+              opacity-0
+              rounded-4px
+              user-teacher
+              md:rounded-4px
+              md:pt-0
+            "
+          ></div>
+          <div
+            class="
+              absolute
+              bottom-0
+              right-0
+              pb-2
+              pr-2
+              transition-opacity
+              duration-300
+              collect
+              z-5
+              w-60px
+              flex
+              row-direction
+              justify-end
+            "
           >
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="far"
+              data-icon="bookmark"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+              class="
+                text-white
+                transition
+                duration-200
+                f-a-i
+                hover:text-yellow-3
+                svg-inline--fa
+                fa-bookmark fa-w-12 fa-lg
+                mr-1
+              "
+            >
+              <path
+                fill="currentColor"
+                d="M336 0H48C21.49 0 0 21.49 0 48v464l192-112 192 112V48c0-26.51-21.49-48-48-48zm0 428.43l-144-84-144 84V54a6 6 0 0 1 6-6h276c3.314 0 6 2.683 6 5.996V428.43z"
+                class=""
+              ></path>
+            </svg>
+            <svg
+              @click="handleCart(course)"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="shopping-cart"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+              class="
+                svg-inline--fa
+                fa-shopping-cart fa-w-18 fa-lg
+                shopping-cart
+                ml-6px
+                transition
+                duration-200
+                hover:text-yellow-3
+                text-white
+                mr-1
+              "
+            >
+              <path
+                fill="currentColor"
+                d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z"
+                class=""
+              ></path>
+            </svg>
+          </div>
         </div>
         <div class="card-content">
-          <div>{{ course.title }}</div>
+          <div>
+            <p class="w-full h-48px line-clamp-2 sn-640:line-clamp-1">
+              {{ course.title }}
+            </p>
+          </div>
           <div class="flex flex-row items-center">
             <div
               class="w-8 h-8 rounded-full card-image bg-center flex-shrink-0"
-              :style="'background-image: url('+course.lecturers[0].avatar+');'"
+              :style="
+                'background-image: url(' + course.lecturers[0].avatar + ');'
+              "
             />
             <div>
-              {{ course.lecturers[0].username }}
+              <p class="pl-2 text-gray-600 line-clamp-1 md:text-14px">
+                {{ course.lecturers[0].username }}
+              </p>
             </div>
           </div>
           <div class="relative md:flex md:justify-between md:h-28px">
-            <div class="flex items-center justify-between text-gray-700 text-14px">
+            <div
+              class="flex items-center justify-between text-gray-700 text-14px"
+            >
               <p class="w-1/2 md:hidden">
-                剩 {{ countDay (course.prices[0].schedule_at) }} 天
-              </p> <p class="w-1/2 text-right md:w-full md:text-left">
-                已募資 103%
+                剩 {{ countDay(course.prices[0].schedule_at) }} 天
               </p>
-            </div> <div class="flex items-center justify-center flex-1 h-28px md:h-full md:w-1/2 md:pl-10px">
+              <p class="w-1/2 text-right md:w-full md:text-left">已募資 103%</p>
+            </div>
+            <div
+              class="
+                flex
+                items-center
+                justify-center
+                flex-1
+                h-28px
+                md:h-full
+                md:w-1/2
+                md:pl-10px
+              "
+            >
               <div class="flex items-center justify-center w-full h-28px">
-                <div
-
-                  class="w-full"
-                >
-                  <div
-
-                    class="flex items-center justify-between"
-                  >
+                <div class="w-full">
+                  <div class="flex items-center justify-between">
                     <div
-
-                      class="w-full overflow-hidden rounded-md h-8px bg-hi-courses-box"
+                      class="
+                        w-full
+                        overflow-hidden
+                        rounded-md
+                        h-8px
+                        bg-hi-courses-box
+                      "
                     >
                       <span
-
                         class="block h-full rounded-md red-rate"
-                        style="width: 103%;"
+                        style="width: 103%"
                       />
                     </div>
                   </div>
@@ -63,39 +162,83 @@
 </template>
 <script>
 export default {
+  props: {
+    cart: {
+      default: [],
+    },
+  },
   data: () => ({
-    courses: []
+    courses: [],
   }),
-  async fetch () {
+  async fetch() {
     this.courses = await this.$axios.$get(
-      'https://api.hiskio.com/v2/courses/fundraising'
-    )
+      "https://api.hiskio.com/v2/courses/fundraising"
+    );
   },
   methods: {
-    countDay (start) {
-      const startDate = new Date(start)
-      const now = new Date()
-      const oneDay = 60 * 60 * 24 * 1000
-      return 30 - Math.ceil((now - startDate) / oneDay)
-    }
-  }
-}
+    countDay(start) {
+      const startDate = new Date(start);
+      const now = new Date();
+      const oneDay = 60 * 60 * 24 * 1000;
+      return 30 - Math.ceil((now - startDate) / oneDay);
+    },
+    handleCart(course) {
+      for (let i = 0; i < this.cart.length; i++) {
+        if (course.id === this.cart[i].id) {
+          this.$emit("remove-course-by-index", i);
+          return;
+        }
+      }
+      this.$emit("add-course", course);
+    },
+  },
+};
 </script>
 <style lang="scss">
 .card-image {
-  background-size: cover
+  background-size: cover;
+}
+.z-5 {
+  z-index: 5;
+}
+.w-60px {
+  width: 60px;
 }
 .h-8px {
-  height: 8px
+  height: 8px;
 }
 .h-28px {
-  height: 28px
+  height: 28px;
+}
+.h-48px {
+  height: 48px;
+}
+.line-clamp-1 {
+  -webkit-line-clamp: 1;
+}
+
+.line-clamp-2 {
+  -webkit-line-clamp: 2;
+}
+
+.line-clamp-1,
+.line-clamp-2 {
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+}
+.hover\:text-yellow-3:hover {
+  --tw-text-opacity: 1;
+  color: rgba(255, 168, 29, var(--tw-text-opacity));
+}
+.svg-inline--fa {
+  width: 1rem;
 }
 .bg-hi-courses-box {
-    --tw-bg-opacity: 1;
-    background-color: rgba(245,245,245,var(--tw-bg-opacity));
+  --tw-bg-opacity: 1;
+  background-color: rgba(245, 245, 245, var(--tw-bg-opacity));
 }
 .red-rate {
-    background-image: linear-gradient(90deg,#eb6767 -.01%,#e34a4a 99.7%);
+  background-image: linear-gradient(90deg, #eb6767 -0.01%, #e34a4a 99.7%);
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,280 @@
 <template>
   <div>
     <Header :is-login="false" />
-    <Fundraising-courses />
+
+    <aside
+      data-v-0d4e163b=""
+      class="
+        fixed
+        top-0
+        flex flex-col
+        items-stretch
+        duration-300
+        bg-white
+        w-375px
+        h-full
+        shadow-aside
+        transition-position
+        z-11
+      "
+      style="right: -375px"
+    >
+      <div class="relative border-gray-400 px-15px py-30px">
+        <p class="text-xl leading-none font-medium text-center text-hi-price">
+          購物車
+        </p>
+        <button
+          class="
+            absolute
+            top-20px
+            right-9px
+            w-40px
+            h-40px
+            text-xl
+            leading-none
+            text-gray-700
+          "
+        >
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="times"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 352 512"
+            class="svg-inline--fa fa-times fa-w-11"
+          >
+            <path
+              fill="currentColor"
+              d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+              class=""
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <div data-v-0d4e163b="" class="scrollbar flex-grow pb-58px overflow-auto">
+        <div data-v-0d4e163b="">
+          <div
+            data-v-0d4e163b=""
+            class="relative border-gray-400 border-b-1 h-115px py-12px px-8px"
+          >
+            <a
+              data-v-0d4e163b=""
+              href="/fundraising/527/about"
+              class="absolute top-0 left-0 w-full h-full z-2"
+              target="_blank"
+            ></a>
+            <div
+              data-v-0d4e163b=""
+              class="relative flex items-center justify-between w-full card"
+            >
+              <div data-v-0d4e163b="" class="relative flex">
+                <div
+                  data-v-0d4e163b=""
+                  class="relative overflow-hidden w-150px rounded-6px"
+                >
+                  <!---->
+                  <!---->
+                  <div
+                    data-v-0d4e163b=""
+                    class="bg-center bg-cover pt-video-top"
+                    data-src="https://thumbnails.f5ezcode.in/eyJidWNrZXQiOiJjZG4uaGlza2lvLmNvbSIsImtleSI6ImNvdXJzZXNcL3Rpb2M5bm1iZG9iYzU0ZiIsImVkaXRzIjp7InBuZyI6eyJxdWFsaXR5IjoxMDB9LCJyZXNpemUiOnsiZml0IjoiY292ZXIiLCJ3aWR0aCI6MzAwfX19"
+                    lazy="loaded"
+                    style="
+                      background-image: url('https://thumbnails.f5ezcode.in/eyJidWNrZXQiOiJjZG4uaGlza2lvLmNvbSIsImtleSI6ImNvdXJzZXNcL3Rpb2M5bm1iZG9iYzU0ZiIsImVkaXRzIjp7InBuZyI6eyJxdWFsaXR5IjoxMDB9LCJyZXNpemUiOnsiZml0IjoiY292ZXIiLCJ3aWR0aCI6MzAwfX19');
+                    "
+                  ></div>
+                </div>
+              </div>
+              <div
+                data-v-0d4e163b=""
+                class="
+                  relative
+                  flex flex-col
+                  items-start
+                  w-3/4
+                  card-content
+                  pl-12px
+                  h-90px
+                "
+              >
+                <div
+                  data-v-0d4e163b=""
+                  class="
+                    flex flex-col
+                    items-start
+                    justify-between
+                    w-full
+                    h-full
+                  "
+                >
+                  <div
+                    data-v-0d4e163b=""
+                    class="flex items-start justify-between w-full"
+                  >
+                    <p
+                      data-v-0d4e163b=""
+                      class="
+                        title
+                        text-16px
+                        w-full
+                        h-48px
+                        break-all
+                        line-clamp-2
+                        text-gray-700
+                      "
+                    >
+                      超新手也能用 Python 爬蟲打造貨比千家的比價網站
+                    </p>
+                    <a data-v-0d4e163b="" class="z-3"
+                      ><svg
+                        data-v-0d4e163b=""
+                        aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fas"
+                        data-icon="trash"
+                        role="img"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 448 512"
+                        class="
+                          text-gray-400
+                          m-3px
+                          mt-5px
+                          svg-inline--fa
+                          fa-trash fa-w-14
+                        "
+                      >
+                        <path
+                          data-v-0d4e163b=""
+                          fill="currentColor"
+                          d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"
+                          class=""
+                        ></path></svg
+                    ></a>
+                  </div>
+                  <div data-v-0d4e163b="" class="flex items-end price -mt-4px">
+                    <div
+                      data-v-0d4e163b=""
+                      class="flex items-end disPrice mr-5px"
+                    >
+                      <span
+                        data-v-0d4e163b=""
+                        class="dollar-sign font-bold mb-2px mr-4px text-red-1"
+                        >$</span
+                      >
+                      <p
+                        data-v-0d4e163b=""
+                        class="
+                          discount-price
+                          text-18px
+                          font-semibold
+                          text-red-1
+                        "
+                      >
+                        2,190
+                      </p>
+                    </div>
+                    <!---->
+                    <!---->
+                    <div
+                      data-v-0d4e163b=""
+                      class="
+                        flex
+                        items-center
+                        justify-center
+                        border-solid border-1
+                        text-14px
+                        rounded-4px
+                        text-hi-price
+                        mb-3px
+                        border-hi-price
+                        w-64px
+                        h-21px
+                      "
+                    >
+                      募資課程
+                    </div>
+                    <!---->
+                    <!---->
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button
+        data-v-0d4e163b=""
+        class="
+          absolute
+          bottom-0
+          w-full
+          rounded-none
+          btn
+          default-solid-btn
+          h-58px
+          z-3
+        "
+      >
+        <svg
+          data-v-0d4e163b=""
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="shopping-cart"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 576 512"
+          class="mr-16px svg-inline--fa fa-shopping-cart fa-w-18 fa-lg"
+        >
+          <path
+            data-v-0d4e163b=""
+            fill="currentColor"
+            d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z"
+            class=""
+          ></path>
+        </svg>
+        <p data-v-0d4e163b="" class="text-18px">前往購物車</p>
+      </button>
+    </aside>
+
+    <Fundraising-courses
+      :cart="cart"
+      @add-course="addCourse"
+      @remove-course-by-index="removeCourseByIndex"
+    />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      cart: [],
+    };
+  },
+  methods: {
+    addCourse(course) {
+      this.cart.push(course);
+      document.getElementsByTagName("aside")[0].style.right = 0;
+    },
+    removeCourseByIndex(index) {
+      this.cart.splice(index, 1);
+    },
+  },
+};
 </script>
+<style lang="scss" scoped>
+.shadow-aside {
+  --tw-shadow: -5px 0 20px rgba(0, 0, 0, 0.05);
+}
+.shadow-aside,
+.shadow-blur {
+  box-shadow: var(--tw-ring-offset-shadow, 0 0 transparent),
+    var(--tw-ring-shadow, 0 0 transparent), var(--tw-shadow);
+}
+.z-11 {
+  z-index: 11;
+}
+</style>
